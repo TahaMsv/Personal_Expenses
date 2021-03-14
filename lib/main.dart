4583e7493a9007288acc739be5d25e3e54e67223
tahamousavi <tahamousavi.sbu@gmail.com>
2021-03-14 13:59:36 +0330

    The display of list items became more complete

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,16 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: transactions.map((tx) {
                   return Container(
-                    child: Card(child: Text(tx.id),),
+                   child: Row(
+                     children:<Widget>[
+                       Container(child: Text(tx.title),),
+                       Container(child: Column(children: <Widget>[
+                         Container(child: Text(tx.id),),
+                         Container(child: Text( '\$${tx.amount}'),),
+              
+                       ],),),
+                     ]
+                   ),
                   );
                 }).toList(),
               ),
