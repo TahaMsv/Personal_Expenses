aadf9d87ba8b57547c431a1f65f50aa48edf8d55
tahamousavi <tahamousavi.sbu@gmail.com>
2021-03-16 21:22:54 +0330

    ListView.Builder added

--- lib/transactions_list.dart
+++ lib/transactions_list.dart
@@ -1,54 +1,58 @@
 import 'package:flutter/material.dart';
 import './Transaction.dart';
 
-class TransactionsList extends StatelessWidget {
- 
+class  TransactionsList extends StatelessWidget {
   final List<Transaction> userTransactions;
   TransactionsList(this.userTransactions);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: userTransactions.map((tx) {
-        return Card(
-          child: Row(children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 15,
-              ),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 2,
-                  color: Colors.purple,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
                 ),
-              ),
-              child: Text(
-                '\$${tx.amount}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.purple,
-                ),
-              ),
-            ),
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.purple,
                   ),
-                ],
+                ),
+                child: Text(
+                  '\$${userTransactions[index].amount}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
               ),
-            ),
-          ]),
-        );
-      }).toList(),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      userTransactions[index].title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          );
+        },
+        itemCount: userTransactions.length,
+      ),
     );
   }
 }
